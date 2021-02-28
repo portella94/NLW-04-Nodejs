0f72534e6f750b2b10d485f108d28f920e8ca544
Chrystian <chrystian100@hotmail.com>
2021-02-28 01:15:33 -0300

    Model and Controller config

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" });
-});
-app.post("/user", (request, response) => {
-    return response.json({ message: "Data saved!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is runnig"));
