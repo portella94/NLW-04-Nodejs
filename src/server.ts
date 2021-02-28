07632231dda766c59ade269f14a84a0cf747f00f
Chrystian <chrystian100@hotmail.com>
2021-02-28 00:35:17 -0300

    DB config and first migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is runnig"));
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World" });
+});
+app.post("/user", (request, response) => {
+    return response.json({ message: "Data saved!" });
+});
 
+app.listen(3333, () => console.log("Server is runnig"));
