a3719318e860e29df1ed3181234b8c4b64a22289
Chrystian <chrystian100@hotmail.com>
2021-02-28 01:50:58 -0300

    Added Show Method

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 router.post("/users", userController.create);
+router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
 
 export { router };
