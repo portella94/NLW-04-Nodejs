9a2865b74eb818a65b8b13db41523b0daabebb4f
Chrystian <chrystian100@hotmail.com>
2021-02-28 03:35:55 -0300

    Survey Integration Test

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import { app } from "../app"
+import createConnection from "../database";
+
+describe("Surveys", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new Survey", async () => {
+        const response = await request(app).post("/surveys")
+            .send({
+                title: "Example",
+                description: "Description Example"
+            });
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+    });
+
+    it("Should be able to get all Surveys", async () => {
+        await request(app).post("/surveys")
+            .send({
+                title: "Example 2",
+                description: "Description Example 2"
+            });
+        const response = await request(app).get("/surveys");
+        expect(response.body.length).toBe(2);
+    });
+});
